1a0e9f0a57dbe89ba6490da196863638e681b02c
Bmxertv_YT <chrissi.klaussxhulz@gmail.com>
2021-11-04 22:14:54 +0100

    Added Method getBanReason from UUID

--- src/main/java/de/bmxertv/easyban/manager/BanManager.java
+++ src/main/java/de/bmxertv/easyban/manager/BanManager.java
@@ -2,14 +2,24 @@ package de.bmxertv.easyban.manager;
 
 import de.bmxertv.easyban.EasyBan;
 import de.bmxertv.easyban.model.BanModel;
+import de.bmxertv.easyban.model.ReasonModel;
+import de.bmxertv.easyban.util.ColorUtils;
 import de.bmxertv.easyban.util.ConsoleUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerLoginEvent;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class BanManager {
 
@@ -23,7 +33,24 @@ public class BanManager {
         return fileConfiguration.getConfigurationSection(uuid.toString()) != null;
     }
 
-    public void saveBan(BanModel model) {
+    public static String getBanReason(UUID uuid) {
+        EasyBan easyBan = EasyBan.getPlugin(EasyBan.class);
+        File file = Paths.get(easyBan.getDataFolder().getAbsolutePath(), "bans.yml").toFile();
+        FileConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
+
+        BanModel banModel = BanModel.deserialize(fileConfiguration.getConfigurationSection(uuid.toString()));
+        ReasonModel reasonModel = ReasonModel.deserialize(easyBan.getConfig().getConfigurationSection("reasons." + banModel.getReasonId()));
+
+        String reason = reasonModel.getMessage()
+                .stream()
+                .collect(Collectors.joining("\n"))
+                .replace("%reason%", banModel.getReason())
+                .replace("%from%",  banModel.getFrom())
+                .replace("%until%", banModel.getBannedUntil().format(DateTimeFormatter.ofPattern(easyBan.getConfig().getString("dateTimeFormate"))));
+        return reason;
+    }
+
+    public void ban(BanModel model) {
         if (!file.exists()) {
             try {
                 file.createNewFile();
@@ -34,6 +61,44 @@ public class BanManager {
 
         FileConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
         model.serialize(fileConfiguration);
+        try {
+            fileConfiguration.save(file);
+        } catch (IOException e) {
+            ConsoleUtil.error("File bans.yml can't save");
+        }
+
+        ReasonModel reasonModel = ReasonModel.deserialize(easyBan.getConfig().getConfigurationSection("reasons." + model.getReasonId()));
+
+        String reason = reasonModel.getMessage()
+                .stream()
+                .collect(Collectors.joining("\n"))
+                .replace("%reason%", model.getReason())
+                .replace("%from%", model.getFrom())
+                .replace("%until%", model.getBannedUntil().format(DateTimeFormatter.ofPattern(easyBan.getConfig().getString("dateTimeFormate"))));
+
+        Bukkit.getPlayer(model.getBannedUuid()).kickPlayer(ColorUtils.colorize(reason));
+    }
+
+    public void unban(UUID uuid) {
+        if (!file.exists()) {
+            try {
+                throw new FileNotFoundException();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        FileConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(file);
+        ConfigurationSection section = fileConfiguration.getConfigurationSection(uuid.toString());
+        if (section == null) {
+            ConsoleUtil.error("UUID can't unbanned because she doesn't exists");
+            return;
+        }
+
+        for (String key : section.getKeys(true)) {
+            section.set(key, null);
+        }
+        fileConfiguration.set(uuid.toString(), null);
         try {
             fileConfiguration.save(file);
         } catch (IOException e) {
